8e3f949b1622e5f993325e0ce884df7ef43766cd
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-09-26 21:08:54 +0300

    password validation added

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+const PSW1 = document.getElementById("password1");
+const PSW2 = document.getElementById("password2");
+const CREATE_ACC_BTN = document.querySelector("button");
+
+function checkPassword(e) {
+  if (PSW1.value != PSW2.value) {
+    document.querySelector(".psw-error").style.display = "block";
+    PSW1.classList.add("psw-error-border");
+    PSW2.classList.add("psw-error-border");
+    console.log("passwords did not match");
+    e.preventDefault();
+  } else document.querySelector(".psw-error").style.display = "none";
+  console.log("passwords OK");
+}
+
+CREATE_ACC_BTN.addEventListener("click", checkPassword);
